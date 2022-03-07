dbfae87710842620e0779067fcbe39a45c3068e2
Difelice-e <difelice.emanuele@gmail.com>
2022-03-07 15:14:20 +0100

    creato titolo

---
+++ src/components/myFirstTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  name: 'myFirstTitle',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h1 {
+    font-size: 2.5rem;
+    color: greenyellow;
+    text-decoration: underscore;
+}
+</style>
